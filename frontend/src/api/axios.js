67cbc00c4acd00aad932271b80e56557d6630c09
Vipul Avinash <vipulavinash123@gmail.com>
2026-06-01 22:15:29 +0530

    Final Commit

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
-
 const api = axios.create({
-    baseURL : import.meta.env.VITE_API_URL || 'http://localhost:5001/api'
+  baseURL: isLocal 
+    ? 'http://localhost:5001/api' 
+    : 'https://e-commerce-project-x82b.onrender.com/api'
 })
 
 export default api
