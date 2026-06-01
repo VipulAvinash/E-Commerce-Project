fd07a26b3c6f640f771790a0a7bd824c0bc839bd
Vipul Avinash <vipulavinash123@gmail.com>
2026-06-01 22:12:27 +0530

    Final Commit

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,28 @@ dotenv.config()
 
 const app = express()
 
-app.use(cors())
+const allowedOrigins = [
+    "https://dashcartt.netlify.app",
+    'http://localhost:5173',
+
+]
+app.use(cors({
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps, Postman, or server-to-server)
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.indexOf(origin) !== -1 || origin.includes('netlify.app')) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+
 app.use(express.json())
 app.use('/api/auth',authRoutes)
 app.use('/api/products',productRoutes)
